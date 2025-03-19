9835fda2f96a2b080d52045c925c250434f83f24
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-03-19 15:54:34 +0200

    Instructiuni SQL

---
+++ CURS 1/2.instructiuni.sql
@@ -0,0 +1,37 @@
+
+show databases;
+
+create database if not exists baza_de_date;
+
+show databases;
+
+use baza_de_date;
+
+# 0 .. 18
+create table if not exists iepuri (nume varchar(255), varsta smallint);
+
+select * from iepuri;
+
+insert into iepuri values ("Bugs", 5);
+
+select * from iepuri;
+
+insert into iepuri set varsta = 9, nume = "Morcoveata";
+
+select * from iepuri where varsta = 5;
+
+select * from iepuri where nume = "Bugs";
+
+
+insert into iepuri values ("Bugs", 7);
+insert into iepuri values ("Bugs", 10);
+insert into iepuri values ("Bugs", 12);
+
+select * from iepuri;
+
+set sql_safe_updates = 0;
+update iepuri set nume = "Bugs Bunny" where nume = "Bugs";
+
+select * from iepuri;
+
+set sql_safe_updates = 1;
