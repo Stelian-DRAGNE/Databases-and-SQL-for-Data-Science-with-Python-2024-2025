ac9c9073d2e8c69b19754f3917c989caea7fcefd
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-03-25 13:32:04 +0200

    Instructiuni introductive SQL

---
+++ CURS 1/intro_SQL.sql
@@ -0,0 +1,45 @@
+show databases;
+-- create database prima_baza_de_date;
+#  show databases;
+
+create database a_doua_baza_de_date;
+show databases;
+
+drop database a_doua_baza_de_date;
+show databases;
+
+create database if not exists a_doua_baza_de_date;
+drop database if exists a_doua_baza_de_date;
+
+create database if not exists a_treia_baza_de_date;
+drop database if exists a_treia_baza_de_date;
+
+show databases;
+
+use prima_baza_de_date;
+
+create table calorii (nr_crt int, calorii int);
+show databases;
+drop table if exists calorii;
+
+create table if not exists calorii (nr_crt int, calorii int);
+
+insert into calorii values (1, 2500);
+select * from calorii;
+
+insert into calorii values (2, 3000);
+select * from calorii;
+
+insert into calorii values (3500, 3);
+select * from calorii;
+
+insert into calorii set nr_crt = 4, calorii = 1900;
+select * from calorii;
+
+insert into calorii set calorii = 2223;
+select * from calorii;
+
+drop table calorii;
+
+insert into calorii set calorii = 2223;
+select * from calorii;
