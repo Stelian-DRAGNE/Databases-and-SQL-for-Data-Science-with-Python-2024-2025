4b47c3cba11003bc988730573c27839ba8e56a56
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-04-01 12:06:17 +0300

    EER

---
+++ CURS 3/facultate_3.sql
@@ -0,0 +1,11 @@
+USE facultate;
+
+SELECT * FROM student;
+SELECT * FROM facultatea;
+
+INSERT INTO facultatea SET nume = 'Vietii', adresa = 'Str. Soarelui';
+SELECT * FROM facultatea;
+
+SELECT * FROM student;
+INSERT INTO student SET nume = 'Andy', prenume = 'Ionut', grupa = 2, email = 'ionut@vietii.ro', facultatea_idfacultate = 1;
+SELECT * FROM student;
---
+++ CURS 3/query_generat_2.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema facultate
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema facultate
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `facultate` DEFAULT CHARACTER SET utf8 ;
+USE `facultate` ;
+
+-- -----------------------------------------------------
+-- Table `facultate`.`student`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `facultate`.`student` (
+  `idstudent` INT NOT NULL AUTO_INCREMENT,
+  `nume` VARCHAR(255) NOT NULL,
+  `prenume` VARCHAR(255) NOT NULL,
+  `grupa` ENUM('1', '2', '3', '4') NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `data_inscrierii` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `statut` ENUM('admis', 'respins', 'neevaluat') NULL DEFAULT 'neevaluat',
+  PRIMARY KEY (`idstudent`),
+  UNIQUE INDEX `nume_UNIQUE` (`nume` ASC) VISIBLE,
+  UNIQUE INDEX `prenume_UNIQUE` (`prenume` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `facultate`.`facultatea`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `facultate`.`facultatea` (
+  `idfacultate` INT NOT NULL AUTO_INCREMENT,
+  `nume` VARCHAR(255) NULL,
+  `adresa` VARCHAR(255) NULL,
+  PRIMARY KEY (`idfacultate`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
