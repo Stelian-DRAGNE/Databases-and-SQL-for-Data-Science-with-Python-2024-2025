994f6f4f87b894b29cda549e0aee7267cbaa8c70
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-05-05 15:25:40 +0300

    Instructiuni SQL

---
+++ CURS 1/test.sql
@@ -0,0 +1,14 @@
+SHOW DATABASES;
+
+USE angajati;
+
+SELECT * from studenti;
+
+INSERT INTO studenti SET nume = "Andreea";
+SELECT * FROM studenti;
+
+INSERT INTO studenti SET nume = "Andreea";
+SELECT * FROM studenti;
+
+DELETE FROM studenti WHERE id = 10;
+SELECT * FROM studenti;
