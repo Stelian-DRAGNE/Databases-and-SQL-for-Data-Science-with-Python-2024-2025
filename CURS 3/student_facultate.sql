9345799b139564f3a39e9e4a250cbb9c7139d145
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-05-07 13:40:18 +0300

    Normalizare

--- CURS 3/student_facultate.sql
+++ CURS 3/student_facultate.sql
@@ -9,3 +9,6 @@ SELECT * FROM facultate;
 SELECT * FROM student;
 INSERT INTO student SET nume = 'Andy', prenume = 'Ionut', grupa = 2, email = 'ionut@vietii.ro', facultate_idfacultate = 1;
 SELECT * FROM student;
+
+INSERT INTO student SET nume = 'Anton', prenume = 'Pan', grupa = 2, email = 'anton@vietii.ro', facultate_idfacultate = 1;
+SELECT * FROM student;
