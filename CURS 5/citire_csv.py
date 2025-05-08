acc1e0a0de856445312d731ed0e30d03ca03ac09
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-05-08 19:41:48 +0300

    Profesori

---
+++ CURS 5/citire_csv.py
@@ -0,0 +1,9 @@
+
+import csv
+
+
+with open("profesori_original.csv", "r") as freader:
+    csv_reader = csv.DictReader(freader)
+    print(csv_reader)
+    for row in csv_reader:
+        print(row)
