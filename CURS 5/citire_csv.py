7354cd58db507a19ecc02164aefa75884a210782
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-05-11 12:30:20 +0300

    Delete CURS 5 directory

--- CURS 5/citire_csv.py
+++
@@ -1,9 +0,0 @@
-
-import csv
-
-
-with open("profesori_original.csv", "r") as freader:
-    csv_reader = csv.DictReader(freader)
-    print(csv_reader)
-    for row in csv_reader:
-        print(row)
